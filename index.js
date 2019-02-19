e165dd3215fba0501a2fc7669224817e9d95a157
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-02-19 17:00:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here
+function greet (time) {
+	let timeInt = parseInt(time.split(':')[0])
+
+	if(timeInt > 12){
+		return 'Good Morning'
+	} else if (timeInt < 5){
+		return 'Good Evening'
+	} else {
+		return 'Good Afternoon'
+	}
+}// Write your code here
