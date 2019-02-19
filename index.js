54437b5f903c3a2511f3679101e511b0bf5e2e37
NorbertLam <noobertlm@gmail.com>
2019-02-19 16:33:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here
+const input = document.querySelector('input#time');
+
+function greet(val) {
+  const hr = parseInt(val.split(":")[0])
+
+  if (hr < 12 ) {
+    return "Good Morning";
+  } else if (hr > 12 && hr < 17)
+    return "Good Afternoon";
+  else {
+    return "Good Evening";
+  }
+}
