0c5e5f6d8322f34a309d15a0d904bb46d5126a3c
rchow1944 <rchow2@buffalo.edu>
2019-02-19 16:22:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here
+function greet(time) {
+  const greeting = document.getElementById('greeting');
+  let timeInt = parseInt(time.split(":")[0]);
+  console.log(timeInt);
+  if (timeInt <= 12) {
+    return "Good Morning";
+  } else if (timeInt > 12 && timeInt < 17) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
