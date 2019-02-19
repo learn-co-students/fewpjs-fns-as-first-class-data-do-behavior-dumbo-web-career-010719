5b185cbcf1cd4446a04149a9c45a6f0e857d3f27
Redmond Chan <redmondchan92@gmail.com>
2019-02-19 16:51:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here
+function greet(input) {
+
+// const input = document.querySelector("input")
+const x = input.split(":")
+const hour = parseInt(x[0])
+
+  if (hour < 12) {
+      return "Good Morning"
+    } else if (12 <= hour && hour <= 17) {
+      return "Good Afternoon"
+    } else {
+      return "Good Evening"
+  }
+};
