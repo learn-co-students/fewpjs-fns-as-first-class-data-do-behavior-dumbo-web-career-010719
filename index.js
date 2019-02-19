8369a491b28a590f64cf96bbd0c3403868d581a0
ali-mohammed200 <ali.mohammed200@gmail.com>
2019-02-19 18:06:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here
+
+function greet(time){
+  if (parseInt(time) < 12){
+    return 'Good Morning';
+  }
+  else if (parseInt(time) > 12 && parseInt(time) < 17) {
+    return 'Good Afternoon'
+  }
+  else {
+    return 'Good Evening'
+  }
+}
+function show(message){
+  let h1 = document.querySelector('h1#greeting');
+  h1.innerText = message;
+}
