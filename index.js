c0c07431770e594e7b83ac27489d0e0ebf3fc633
github username <github email address>
2019-02-19 18:03:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here
+function greet(time) {
+  let greet;
+  time = parseInt(time)
+  if (time < 12){
+    return "Good Morning"}
+  else if (time >= 12 && time <= 17){
+    return "Good Afternoon"}
+  else if (time > 17 && time <= 24){
+    return "Good Evening"}
+  else{
+    return "Not a valid time"}
+  end
+  document.getElementById('greeting').innerText = greet
+  return greet
+}
