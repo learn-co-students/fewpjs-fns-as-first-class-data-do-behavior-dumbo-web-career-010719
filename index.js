7a240f172d80373efea203aa48922e3c88009a7f
Your Name <you@example.com>
2019-02-20 15:37:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here
+
+function greet(time) {
+  let greet;
+  time = parseInt(time);
+
+  if (time < 12) {
+    greet = "Good Morning";
+  } else if (time >= 12 && time <= 17) {
+    greet = "Good Afternoon";
+  } else if (time > 17 && time <= 24) {
+    greet = "Good Evening";
+  } else {
+    greet = "Hello";
+  }
+
+  document.getElementById("greeting").innerHTML = greet;
+  return greet;
+}
